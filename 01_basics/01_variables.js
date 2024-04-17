f69c1ec7fb061ff99860b74d4c9f670d3d400467
AbuHurairaN <165356288+AbuHurairaN@users.noreply.github.com>
2024-04-17 17:04:28 +0000

    variable js done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1434
+let accountName = "NHuraira"
+var accountPassword = "nhuraira12345"
+accountCity = "Lahore"
+
+accountCity = "Multan"
+accountName = "Warzone"
+accountPassword = "4312223"
+console.log(accountId)
+
+console.table ([accountName, accountPassword, accountCity])
