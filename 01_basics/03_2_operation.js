03ec572a283a48365bcf1debce53c4f140bb7641
AbuHurairaN <165356288+AbuHurairaN@users.noreply.github.com>
2024-04-18 16:15:34 +0000

    conversion and operations done

---
+++ 01_basics/03_2_operation.js
@@ -0,0 +1,8 @@
+let name1 = "hello"
+let name2 = " Huraira"
+name3 = name1 + name2
+console.log(name3);
+
+console.log((1+2)*2)
+console.log(1+2+"2")
+console.log("1"+2+2)
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,6 @@
+let actualName = "huraira"
+//console.log(typeof "huraira")
+
+let myName = Number(actualName)
+console.log(typeof myName);
+console.log(myName)
