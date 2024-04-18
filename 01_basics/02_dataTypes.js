eafa185409722b77b6b1c7befd6e5809b346d4b2
AbuHurairaN <165356288+AbuHurairaN@users.noreply.github.com>
2024-04-18 14:04:28 +0000

    datatype done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+let name = "NHuraira"
+myAge = 23
+registerUser = true
+myMarks = 1000
+future = undefined
+
+console.log(typeof "NHuraira")
+console.log(typeof 23)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof 1000)
