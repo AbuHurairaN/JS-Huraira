661cb6ab4b7193a6b2baa730ad6e4201996d98c2
AbuHurairaN <165356288+AbuHurairaN@users.noreply.github.com>
2024-05-01 09:43:03 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+//simple comparisons
+// ==, <=, >=, =, >, <
+//console.log(2 = 1);
+
+//when compare null with zero always the boolean value comes to false 
+//except the case of
+console.log(null >= 0); // true
+console.log(null == 0); //false
+//cuz the comparisons convert the null into a number treating it as 0.
+
+//SAME AS UNDEFINED
+
+//In case of === the comparison not only check the values but also check out their data type.
+console.log("2" === 1);
